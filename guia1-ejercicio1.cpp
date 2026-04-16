2fe798a6deb5ae374cd6f2a88308a773bf8787cc
palacioss18 <josepaalaacioss@gmail.com>
2026-04-16 10:42:11 -0300

    Add basic program to sum two numbers

---
+++ guia1-ejercicio1.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int numero1,numero2;
+    cout<<"Ingresa 2 numeros"<<endl;
+    cout<<"primer numero: ";
+    cin>>numero1;
+    cout<<"segundo numero: ";
+    cin>>numero2;
+    cout<<"La suma es: "<<numero1 + numero2;
+}
