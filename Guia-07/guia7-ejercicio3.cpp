8de3faab0f5f7b1e90cb4dd4a7feea5d700a1086
Palacios <josepaalaacioss@gmail.com>
2026-04-24 23:35:13 -0300

    Add program to print even numbers from 1 to 20
    Implement a program to iterate and print even numbers from 1 to 20.

---
+++ Guia-07/guia7-ejercicio3.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+int main(){
+    //itera numeros pares
+    for(int i = 1; i <= 20;i++){
+        if(i % 2 == 0){
+            std::cout<<"Numeros pares: "<<i<<std::endl;
+        }
+        else{
+        }
+
+    }
+}
