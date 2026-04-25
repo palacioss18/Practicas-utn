37c94d0c1e4022606e2a5540269ebac3790b9f3e
Palacios <josepaalaacioss@gmail.com>
2026-04-24 23:40:57 -0300

    Create program to print odd numbers
    Add a program to print odd numbers from 1 to 15.

---
+++ Guia-07/guia7-ejercicio6.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+int main(){
+    //Imprime solo numeros impares
+    for(int i = 1; i <= 15;i++){
+        if(i % 2 !=0){
+            std::cout<<"Numero: "<<i<<std::endl;
+        }
+        else{
+        }
+    }
+}
